f50a796fc0108cfec517def8fc2d5e4c749712d0
nielsep <pedro.nielsen.rotta@gmail.com>
2019-04-10 16:09:20 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,25 @@
 
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, request, Response, render_template
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 app = Flask(__name__)
 
-@app.route('/forecast/<int:store_number>/<int:item_number>/')
+@app.route('/forecast/<int:store_number>/<int:item_number>/<initial_date>/<final_date>')
+def forecast(store_number, item_number, initial_date, final_date):
+
+    date_range = pd.date_range(pd.to_datetime(initial_date), pd.to_datetime(final_date))
+    forecast_model_results = pd.DataFrame({'STORE' : store_number, 'ITEM' : item_number, 'DATE' : date_range,\
+                                           'MODEL' : 'Facebook Prophet',
+                                           'POINT_FORECAST' : np.random.randint(low=15, high=30, size=len(date_range))})
+
+    return render_template("forecast.html",  data=forecast_model_results.to_html())
+
+@app.route('/plot/<int:store_number>/<int:item_number>/')
 def forecast_hw(store_number, item_number):
-    # show the user with the given id, the id is an integer
-    forecast_values = [12, 4, 6]
-    #return 'Store {}, item {}, forecast values are {}'.format(store_number,item_number,forecast_values)
-    return jsonify({
-        'store': store_number,
-        'item': item_number,
-        'forecast': forecast_values
-    })
+
+    sales = np.random.rand(50)
+    plt.plot(sales)
+
+    return render_template('WebPage1.html', name = plt.show())
