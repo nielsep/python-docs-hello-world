a4cea519c6f3bc8299c47d7b39e39e1ada35beaf
nielsep <pedro.nielsen.rotta@gmail.com>
2019-04-10 13:12:08 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,14 @@
 
-from flask import Flask
+from flask import Flask, jsonify, request, Response
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, Built-in"
+@app.route('/forecast/<int:store_number>/<int:item_number>/')
+def forecast_hw(store_number, item_number):
+    # show the user with the given id, the id is an integer
+    forecast_values = [12, 4, 6]
+    #return 'Store {}, item {}, forecast values are {}'.format(store_number,item_number,forecast_values)
+    return jsonify({
+        'store': store_number,
+        'item': item_number,
+        'forecast': forecast_values
+    })
