575769b29b3528fd2335af76a0f07029c475c466
nielsep <pedro.nielsen.rotta@gmail.com>
2019-04-10 17:34:09 -0700

    Update application.py

--- application.py
+++ application.py
@@ -3,23 +3,30 @@ from flask import Flask, jsonify, request, Response, render_template
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
+
+def build_graph(values):
+    img = io.BytesIO()
+    plt.plot(values)
+    plt.savefig(img, format='png')
+    img.seek(0)
+    graph_url = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+    return 'data:image/png;base64,{}'.format(graph_url)
 
 @app.route('/forecast/<int:store_number>/<int:item_number>/<initial_date>/<final_date>')
 def forecast(store_number, item_number, initial_date, final_date):
 
     date_range = pd.date_range(pd.to_datetime(initial_date), pd.to_datetime(final_date))
+    point_forecast = np.random.randint(low=15, high=30, size=len(date_range))
+    graph1_url = build_graph(point_forecast)
+
     forecast_model_results = pd.DataFrame({'STORE' : store_number, 'ITEM' : item_number, 'DATE' : date_range,\
                                            'MODEL' : 'Facebook Prophet',
-                                           'POINT_FORECAST' : np.random.randint(low=15, high=30, size=len(date_range))})
+                                           'POINT_FORECAST' : point_forecast})
 
-    return render_template("forecast.html",  data=forecast_model_results.to_html())
+    return render_template("forecast.html",  data=forecast_model_results.to_html(), graph1=graph1_url)
 
-@app.route('/plot/<int:store_number>/<int:item_number>/')
-def forecast_hw(store_number, item_number):
-
-    sales = np.random.rand(50)
-    plt.plot(sales)
-
-    return render_template('WebPage1.html', name = plt.show())
