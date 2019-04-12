5a11d919f1cffc81a216e3fce8decbafd6bdaf1a
nielsep <pedro.nielsen.rotta@gmail.com>
2019-04-12 12:14:54 -0700

    Update application.py

--- application.py
+++ application.py
@@ -16,7 +16,8 @@ def build_graph(values):
     plt.close()
     return 'data:image/png;base64,{}'.format(graph_url)
 
-@app.route('/forecast/<int:store_number>/<int:item_number>/<initial_date>/<final_date>')
+### Version 1: Forecast from query-string request
+@app.route('/forecastv1/<int:store_number>/<int:item_number>/<initial_date>/<final_date>')
 def forecast_web(store_number, item_number, initial_date, final_date):
 
     date_range = pd.date_range(pd.to_datetime(initial_date), pd.to_datetime(final_date))
@@ -27,8 +28,35 @@ def forecast_web(store_number, item_number, initial_date, final_date):
                                            'MODEL' : 'Facebook Prophet',
                                            'POINT_FORECAST' : point_forecast})
 
-    return render_template("forecast.html",  data=forecast_model_results.to_html(), graph1=graph1_url)
+    return render_template("forecastv1.html",  data=forecast_model_results.to_html(), graph1=graph1_url)
 
+### Version 2: Forecast from submitted form
+@app.route('/forecastv2/request')
+def student():
+   return render_template('forecastv2request.html')
+
+@app.route('/forecastv2/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+
+      store_number = request.form['Store']
+      item_number = request.form['Item']
+      initial_date = request.form['InitialDate']
+      final_date = request.form['FinalDate']
+
+      #store_number = '13'; item_number = '32'; initial_date = '2018-01-01'; final_date = '2018-01-05'
+
+      date_range = pd.date_range(pd.to_datetime(initial_date), pd.to_datetime(final_date))
+      point_forecast = np.random.randint(low=15, high=30, size=len(date_range))
+      graph1_url = build_graph(point_forecast)
+
+      forecast_model_results = pd.DataFrame({'STORE' : store_number, 'ITEM' : item_number, 'DATE' : date_range,\
+                                             'MODEL' : 'Facebook Prophet', 'POINT_FORECAST' : point_forecast})
+
+   return render_template("forecastv2result.html",  data=forecast_model_results.to_html(), graph1=graph1_url)
+
+
+### Version 3: Forecast API returning json file
 @app.route('/API/forecast/<int:store_number>/<int:item_number>/<initial_date>/<final_date>')
 def forecast_api(store_number, item_number, initial_date, final_date):
 
@@ -39,4 +67,5 @@ def forecast_api(store_number, item_number, initial_date, final_date):
                                            'MODEL' : 'Facebook Prophet',
                                            'POINT_FORECAST' : point_forecast})
 
-    return forecast_model_results.to_json(orient='split')
+    return forecast_model_results.to_json(orient='index')
+
